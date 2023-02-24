b610035796eca4883ce64b76766f59d129eeaf6e
בני סולומון <mr.benny.solomon@gmail.com>
2023-02-24 16:35:05 +0200

    match-finding-app ver 1.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import {useRef, useState} from 'react';
+import images from './utils/data';
+var i = 0;
 
 function App() {
+  const [image, setImage] = useState(images[0]);
+  const [counter, setCounter] = useState(0);
+  const h1Ref = useRef();
+  const btnGoodRef = useRef();
+  const btnBadRef = useRef();
+
+  console.log(i)
+  
+  const clickHandler = (e) => {
+    if (i < 8) {
+      const answer = e.target.getAttribute('class');
+      console.log('answer', answer);
+      console.log('image.answer', image.answer);
+
+      if (answer === image.answer) {
+        setCounter((prev) => prev + 1);
+      }
+      setImage(images[++i]);
+    }else {
+      if (counter >= 5) {
+        h1Ref.current.innerText = 'WINNER';
+        h1Ref.current.style.color = 'green';
+
+      }else{
+        h1Ref.current.innerText = 'LOSER';
+        h1Ref.current.style.color = 'red';
+      }
+      btnGoodRef.current.enabled = false;
+      btnBadRef.current.enabled = false;
+    }
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <h1 ref={h1Ref}>Match Finding App</h1>
+        <h3><span>good answers: </span>{counter}</h3>
+      </nav>
+      <main className={image.real}>
+      </main>
+      <div className='description'>{image.desc}</div>
+      <footer>
+        <button ref={btnBadRef} onClick={clickHandler} className='bad'></button>
+        <button ref={btnGoodRef} onClick={clickHandler} className='good'></button>
+      </footer>
     </div>
   );
 }
---
+++ src/utils/data.js
@@ -0,0 +1,13 @@
+const images = [
+  {url: './img/bird.jpg', real: 'bird', desc: 'monkey', answer: 'bad'}, 
+  {url: './img/butterflies.jpg', real: 'butterflies', desc: 'butterflies', answer: 'good'}, 
+  {url: './img/cat.jpg', real: 'cat', desc: 'dog', answer: 'bad'}, 
+  {url: './img/fox.jpg', real: 'fox', desc: 'fox', answer: 'good'}, 
+  {url: './img/giraff.jpg', real: 'giraff', desc: 'horse', answer: 'bad'}, 
+  {url: './img/horse.jpg', real: 'horse', desc: 'horse', answer: 'good'}, 
+  {url: './img/owl.jpg', real: 'owl', desc: 'panter', answer: 'bad'}, 
+  {url: './img/swans.jpg', real: 'swans', desc: 'swans', answer: 'good'}, 
+  {url: './img/zebra.jpg', real: 'zebra', desc: 'lion', answer: 'bad'}
+];
+
+export default images;
